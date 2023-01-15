bde28ebef8e419e08beda32ba34fe25164a4f539
山本建治 <kenjibasketball0520@gmail.com>
2023-01-15 19:59:44 +0900

    add:until_lesson27

---
+++ src/features/auth/Auth.tsx
@@ -0,0 +1,89 @@
+import React from "react";
+import { AppDispatch } from "../../app/store";
+import { useSelector, useDispatch } from "react-redux";
+import styles from "./Auth.module.css";
+import Modal from "react-modal";
+import { Formik } from "formik";
+import * as Yup from "yup";
+import { TextField, Button, CircularProgress } from "@material-ui/core";
+
+import {
+  selectIsLoadingAuth,
+  selectOpenSignIn,
+  selectOpenSignUp,
+  setOpenSignIn,
+  resetOpenSignIn,
+  setOpenSignUp,
+  resetOpenSignUp,
+  fetchCredStart,
+  fetchCredEnd,
+  fetchAsyncLogin,
+  fetchAsyncRegister,
+  fetchAsyncGetMyProf,
+  fetchAsyncGetProfs,
+  fetchAsyncCreateProf,
+} from "./authSlice";
+
+const customStyles = {
+  overlay: {
+    backgroundColor: "#777777",
+  },
+  content: {
+    top: "55%",
+    left: "50%",
+
+    width: 280,
+    height: 350,
+    padding: "50px",
+
+    transform: "translate(-50%, -50%)",
+  },
+};
+
+const Auth: React.FC = () => {
+  Modal.setAppElement("#root");
+  const openSignIn = useSelector(selectOpenSignIn);
+  const openSignUp = useSelector(selectOpenSignUp);
+  const IsLoadingAuth = useSelector(selectIsLoadingAuth);
+  const dispatch: AppDispatch = useDispatch();
+
+  return (
+    <>
+      <Modal
+        isOpen={openSignUp}
+        onRequestClose={async () => {
+          await dispatch(resetOpenSignUp());
+        }}
+        style={customStyles}
+      >
+        <Formik
+          initialErrors={{ email: "required" }}
+          initialValues={{ email: "", password: "" }}
+          onSubmit={async (values) => {
+            await dispatch(fetchCredStart());
+            const resultReg = await dispatch(fetchAsyncRegister(values));
+
+            if (fetchAsyncRegister.fulfilled.match(resultReg)) {
+              await dispatch(fetchAsyncLogin(values));
+              await dispatch(fetchAsyncCreateProf({ nickName: "anonymous" }));
+
+              await dispatch(fetchAsyncGetProfs());
+              await dispatch(fetchAsyncGetMyProf());
+            }
+
+            await dispatch(fetchCredEnd());
+            await dispatch(resetOpenSignUp());
+          }}
+          validationSchema={Yup.object().shape({
+            email: Yup.string()
+              .email("email format is wrong")
+              .required("email is must"),
+            passWord: Yup.string().required("password is must").min(4),
+          })}
+        ></Formik>
+      </Modal>
+    </>
+  );
+};
+
+export default Auth;
---
+++ src/features/auth/authSlice.ts
@@ -0,0 +1,179 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { RootState } from "../../app/store";
+import axios from "axios";
+import { PROPS_AUTHEN, PROPS_PROFILE, PROPS_NICKNAME } from "../types";
+// import { upload } from "@testing-library/user-event/dist/types/utility";
+
+const apiUrl = process.env.REACT_APP_DEV_API_URL;
+
+export const fetchAsyncLogin = createAsyncThunk(
+  "auth/post",
+  async (authen: PROPS_AUTHEN) => {
+    const res = await axios.post(`${apiUrl}authen/jwt/create`, authen, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    return res.data;
+  }
+);
+
+export const fetchAsyncRegister = createAsyncThunk(
+  "auth/register",
+  async (auth: PROPS_AUTHEN) => {
+    const res = await axios.post(`${apiUrl}api/register`, auth, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    return res.data;
+  }
+);
+
+export const fetchAsyncCreateProf = createAsyncThunk(
+  "profile/post",
+  async (nickName: PROPS_NICKNAME) => {
+    const res = await axios.post(`${apiUrl}api/register`, nickName, {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `JWT ${localStorage.localJWT}`,
+      },
+    });
+    return res.data;
+  }
+);
+
+export const fetchAsyncUpdateProf = createAsyncThunk(
+  "profile/put",
+  async (profile: PROPS_PROFILE) => {
+    const uploadData = new FormData();
+    uploadData.append("nickName", profile.nickName);
+    profile.img && uploadData.append("img", profile.img, profile.img.name);
+    const res = await axios.put(
+      `${apiUrl}api/profile/${profile.id}/`,
+      uploadData,
+      {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `JWT ${localStorage.localJWT}`,
+        },
+      }
+    );
+    return res.data;
+  }
+);
+
+export const fetchAsyncGetMyProf = createAsyncThunk("profile/get", async () => {
+  const res = await axios.get(`${apiUrl}api/myprofile`, {
+    headers: {
+      Authorization: `JWT ${localStorage.localJWT}`,
+    },
+  });
+  return res.data[0];
+});
+
+export const fetchAsyncGetProfs = createAsyncThunk("profiles/get", async () => {
+  const res = await axios.get(`${apiUrl}api/myprofile`, {
+    headers: {
+      Authorization: `JWT ${localStorage.localJWT}`,
+    },
+  });
+  return res.data;
+});
+
+export const authSlice = createSlice({
+  name: "auth",
+  initialState: {
+    openSignIn: true,
+    openSignUp: false,
+    openProfile: false,
+    isLoadingAuth: false,
+    myProfile: {
+      id: 0,
+      nickName: "",
+      userProfile: 0,
+      created_on: "",
+      img: "",
+    },
+    profiles: [
+      {
+        id: 0,
+        nickName: "",
+        userProfile: 0,
+        created_on: "",
+        img: "",
+      },
+    ],
+  },
+  reducers: {
+    fetchCredStart(state) {
+      state.isLoadingAuth = false;
+    },
+    fetchCredEnd(state) {
+      state.isLoadingAuth = true;
+    },
+    setOpenSignIn(state) {
+      state.openSignIn = false;
+    },
+    resetOpenSignIn(state) {
+      state.openSignIn = true;
+    },
+    setOpenSignUp(state) {
+      state.openSignUp = true;
+    },
+    resetOpenSignUp(state) {
+      state.openSignUp = false;
+    },
+    setOpenProfile(state) {
+      state.openProfile = true;
+    },
+    resetOpenProfile(state) {
+      state.openProfile = false;
+    },
+    editNickName(state, action) {
+      state.myProfile.nickName = action.payload;
+    },
+  },
+  extraReducers: (builder) => {
+    builder.addCase(fetchAsyncLogin.fulfilled, (state, action) => {
+      localStorage.setItem("localJWT", action.payload.access);
+    });
+    builder.addCase(fetchAsyncCreateProf.fulfilled, (state, action) => {
+      state.myProfile = action.payload;
+    });
+    builder.addCase(fetchAsyncGetMyProf.fulfilled, (state, action) => {
+      state.myProfile = action.payload;
+    });
+    builder.addCase(fetchAsyncGetProfs.fulfilled, (state, action) => {
+      state.profiles = action.payload;
+    });
+    builder.addCase(fetchAsyncUpdateProf.fulfilled, (state, action) => {
+      state.myProfile = action.payload;
+      state.profiles = state.profiles.map((prof) =>
+        prof.id === action.payload.id ? action.payload : prof
+      );
+    });
+  },
+});
+
+export const {
+  fetchCredStart,
+  fetchCredEnd,
+  setOpenSignIn,
+  resetOpenSignIn,
+  setOpenSignUp,
+  resetOpenSignUp,
+  setOpenProfile,
+  resetOpenProfile,
+  editNickName,
+} = authSlice.actions;
+
+export const selectIsLoadingAuth = (state: RootState) =>
+  state.auth.isLoadingAuth;
+export const selectOpenSignUp = (state: RootState) => state.auth.openSignUp;
+export const selectOpenSignIn = (state: RootState) => state.auth.openSignIn;
+export const selectOpenProfile = (state: RootState) => state.auth.openProfile;
+export const selectProfiles = (state: RootState) => state.auth.profiles;
+export const selectMyProfiles = (state: RootState) => state.auth.myProfile;
+
+export default authSlice.reducer;
